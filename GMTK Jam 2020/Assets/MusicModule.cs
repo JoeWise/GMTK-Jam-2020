6d3978579354896b1b2f7370437c6e4a1a888261
Joe Wise <joedavidwise@gmail.com>
2020-07-11 13:13:46 -0700

    starting piano module logic

---
+++ GMTK Jam 2020/Assets/MusicModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicModule : MonoBehaviour
+{
+    public static AudioSource pianoSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
