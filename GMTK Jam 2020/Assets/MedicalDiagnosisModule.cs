4b9711db95cd5b716b2c3ba486af49fd6dade989
jorotroid <joethered@gmail.com>
2020-07-11 12:46:30 -0700

    Art push - 8 module Icons, 6 skin conditions,  Human profile, and a Piano

---
+++ GMTK Jam 2020/Assets/MedicalDiagnosisModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicalDiagnosisModule : MonoBehaviour, TimoModule
+{
+    public bool taskInQueue = false;
+    public Task t;
+    public float taskLength = 5.0f;
+
+    public TextMesh text;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!taskInQueue)
+        {
+            
+        }
+    }
+
+    public void AddTaskToQueue(Task t)
+    {
+        TaskManager.Instance.AddTask(t);
+    }
+
+    public void OnTaskSatisfied()
+    {
+        t.completed = true;
+        taskInQueue = false;
+    }
+}
