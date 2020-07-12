f00920b4c74833ea0d7a8686f1f3f3c8deced50d
Joe Wise <joedavidwise@gmail.com>
2020-07-12 07:37:18 -0700

    Camera module implemented It's light out.

---
+++ GMTK Jam 2020/Assets/TakePicture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakePicture : MonoBehaviour
+{
+    public CatEnvironment catEnvironment;
+    public CameraModule cameraModule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TryToTakePic()
+    {
+        if (catEnvironment.catVisible)
+        {
+            cameraModule.picTaken = true;
+        }
+    }
+}
